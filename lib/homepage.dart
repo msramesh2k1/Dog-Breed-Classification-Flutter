dfbd6f503634ba9231cfa769198bfd2c44a21812
Ramesh M S <msramesh2k1@gmail.com>
2022-12-15 21:42:07 +0530

    tflite func

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,7 +1,10 @@
+import 'package:camera/camera.dart';
+import 'package:dog_breed_classification/main.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_tflite/flutter_tflite.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -11,18 +14,88 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  bool isWorking = false;
+  String result = '';
+  CameraController? cameraController;
+  CameraImage? imgCamera;
+
+  initCamera() {
+    cameraController = CameraController(cameras![0], ResolutionPreset.medium);
+    cameraController!.initialize().then((value) {
+      if (!mounted) {
+        return;
+      }
+      setState(() {
+        cameraController!.startImageStream((image) => {
+              if (!isWorking)
+                {
+                  isWorking = true,
+                  imgCamera = image,
+                  runmodelOnStreamFrames(),
+                }
+            });
+      });
+    });
+  }
+
+  loadmodal() async {
+    Tflite.loadModel(model: "assets/model.tflite", labels: "assets/labels.txt");
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    loadmodal();
+  }
+
+  runmodelOnStreamFrames() async {
+    if (imgCamera != null) {
+      var recognitions = await Tflite.runModelOnFrame(
+        bytesList: imgCamera!.planes.map((plane) {
+          return plane.bytes;
+        }).toList(),
+        imageHeight: imgCamera!.height,
+        imageWidth: imgCamera!.width,
+        imageMean: 127.5,
+        imageStd: 175.5,
+        rotation: 90,
+        numResults: 2,
+        threshold: 0.1,
+        asynch: true,
+      );
+      result = '';
+      recognitions!.forEach((response) {
+        result += response['label'] +
+            " " +
+            (response['confidence'] as double).toStringAsFixed(2) +
+            "\n\n";
+      });
+      setState(() {
+        result;
+      });
+      isWorking = false;
+    }
+  }@override
+  void dispose() async {
+    // TODO: implement dispose
+    super.dispose();
+    await Tflite.close();
+    cameraController!.dispose();
+
+  }
+  
+
   @override
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
-          color: Colors.green[200],
+        color: Colors.green[200],
         image: DecorationImage(
-      
-          image: AssetImage(
-              'assets/back.jpg'),
+          image: AssetImage('assets/back.jpg'),
           fit: BoxFit.fill,
-        ),),
-    
+        ),
+      ),
     );
   }
 }
